ee4b59ef8318715676c85616758d19ab53a3582c
PhilGeelhoed <philgeelhoed@gmail.com>
2020-07-31 08:52:58 +0200

    Add mobile javascript version

--- ab/js/menubar.js
+++ ab/js/menubar.js
@@ -6,17 +6,19 @@ $(document).ready(function() {
     const disclaimerHeight = document.getElementById('disclaimer').getBoundingClientRect().bottom;
     const homeHeight = parallaxx.getBoundingClientRect().top;
     const pandOffset = pandheight-productenheight;
-// const videoparallax = document.getElementById('kantband-video');
-    console.log("disclaimer ehight:  " + disclaimerHeight);
 
-    window.addEventListener("scroll", function() {
-        let offset = window.pageYOffset;
-        parallaxx.style.backgroundPositionY = -172 + offset * 0.8 + "px";
-        let factorOffset = 0.9*offset;
-        let relativeOffset = disclaimerHeight-homeHeight;
-        console.log(relativeOffset);
-        //pandparallax.style.backgroundPositionY = (disclaimerHeight - 250 + factorOffset) + "px";
-        // videoparallax.style.transform = "translate( 0px," + offset * 0.15 + "px)";
-    })
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        // some code..
+    }else
+    {
+        window.addEventListener("scroll", function() {
+            let offset = window.pageYOffset;
+            parallaxx.style.backgroundPositionY = -172 + offset * 0.8 + "px";
+            let factorOffset = 0.9*offset;
+            let relativeOffset = disclaimerHeight-homeHeight;
+
+        })
+    }
+
+
 })
-
