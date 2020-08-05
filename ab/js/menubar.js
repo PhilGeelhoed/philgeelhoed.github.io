6162a75c264b8b9bdc94fb69d7bef110d4bb1bc3
PhilGeelhoed <philgeelhoed@gmail.com>
2020-08-05 12:21:15 +0200

    Javascript cleaning

--- ab/js/menubar.js
+++ ab/js/menubar.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
     const parallaxx = document.getElementById("parallax-background");
-    const pandparallax = document.getElementById('parallax-background2');
-    const pandheight = pandparallax.getBoundingClientRect().top
-    const productenheight = document.getElementById('producten').getBoundingClientRect().top;
-    const disclaimerHeight = document.getElementById('disclaimer').getBoundingClientRect().bottom;
-    const homeHeight = parallaxx.getBoundingClientRect().top;
-    const pandOffset = pandheight-productenheight;
 
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         // some code..
@@ -14,8 +8,6 @@ $(document).ready(function() {
         window.addEventListener("scroll", function() {
             let offset = window.pageYOffset;
             parallaxx.style.backgroundPositionY = -172 + offset * 0.8 + "px";
-            let factorOffset = 0.9*offset;
-            let relativeOffset = disclaimerHeight-homeHeight;
 
         })
     }
